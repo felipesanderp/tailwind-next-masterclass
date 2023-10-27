ed17d300f267c9605cde146bb8cff9622f884341
felipesanderp <felipe.panisson@hotmail.com>
2023-10-27 11:40:23 -0300

    app layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,13 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
