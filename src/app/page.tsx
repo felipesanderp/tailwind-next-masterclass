f40d6907f8b34e0007b84150499fbebf40294721
felipesanderp <felipe.panisson@hotmail.com>
2023-10-27 15:44:23 -0300

    tabs with radix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Hello Tailwind</h1>
+  return (
+    <>
+      <h1 className="tex-3xl font-medium text-zinc-900">Setting</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
