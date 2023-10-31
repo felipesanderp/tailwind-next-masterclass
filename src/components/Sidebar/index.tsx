3400c79e4a2319894ddb673d04e94eb0642f107b
felipesanderp <felipe.panisson@hotmail.com>
2023-10-31 12:17:05 -0300

    fix sidebar responsive

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -22,7 +22,7 @@ import { Button } from '../button'
 
 export function Sidebar() {
   return (
-    <Collapsible.Root className="lg: fixed left-0 top-0 z-20 flex h-screen flex-col gap-6 border-b border-zinc-200 bg-white px-5 py-8 data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen">
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
       <div className="flex items-center justify-between">
         <Logo />
         <Collapsible.Trigger asChild className="lg:hidden">
